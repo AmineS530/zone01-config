cd440d4b6dd3fcc5a045b24bd0708a719bdc8814
Amine Sadik <sadik13saduk@gmail.com>
2024-05-08 18:14:29 +0100

    quick_add-commit-push alias

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-image_path="$HOME/zone01-config/1346622.jpeg"
-git clone https://github.com/AmineS530/zone01-config.git ~/zone01-config && cd ~/zone01-config/ && mv .p10k.zsh ~/.p10k.zsh && mv .zshrc ~/.zshrc && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+image_path="$HOME/zone01-config/Background.jpeg"
+git clone https://github.com/AmineS530/zone01-config.git ~/zone01-config && cd ~/zone01-config/ && mv .p10k.zsh ~/.p10k.zsh && mv .zshrc ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+zsh git_setup.sh
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}"
 gsettings set org.gnome.desktop.background picture-uri "file://${image_path}"
