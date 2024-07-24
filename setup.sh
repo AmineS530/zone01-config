6c290e91e3fbbacca352199878331366e61f53d2
Amine Sadik <105748913+AmineS530@users.noreply.github.com>
2024-07-24 12:27:37 +0100

    uwu

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ image_path="$HOME/zone01-config/Background.jpeg"
 theme_color='Yaru-viridian-dark'
 
 # Clones the settings repo
+if [ -d "~/zone01-config/" ]; then
+    echo -e "Directory ~/zone01-config/ already exists. Overwriting..."
+    # Remove existing directory and clone the repository
+    rm -rf "~/zone01-config/"
+fi
 git clone https://github.com/AmineS530/zone01-config.git ~/zone01-config && cd ~/zone01-config/ 
 
 #Move the premade p10k settings and zshrc
@@ -18,11 +23,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 zsh git_setup.sh
 
 # applies the background on both light and dark mode
-gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}"
-gsettings set org.gnome.desktop.background picture-uri "file://${image_path}"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}" 2>/dev/null
+gsettings set org.gnome.desktop.background picture-uri "file://${image_path}" 2>/dev/null
 
 # Changes theme Color
-gsettings set org.gnome.desktop.interface gtk-theme $theme_color
+gsettings set org.gnome.desktop.interface gtk-theme $theme_color 2>/dev/null
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' 2>/dev/null
 
 # change display and terminal font
 zsh set_font.sh
