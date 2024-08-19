b9534419929203b779beb185ff567a563acfd66b
Amine Sadik <105748913+AmineS530@users.noreply.github.com>
2024-08-19 11:44:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,17 +22,11 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 # small script to set up git for first use and to rembember your info in the future
 zsh git_setup.sh
 
-#change dock to panel mode and enable auto-hide
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false > /dev/null 2>&1
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false > /dev/null 2>&1
-
-# applies the background on both light and dark mode
-gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}" 2>/dev/null
-gsettings set org.gnome.desktop.background picture-uri "file://${image_path}" 2>/dev/null
-
 # Changes theme Color
-gsettings set org.gnome.desktop.interface gtk-theme $theme_color 2>/dev/null
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' 2>/dev/null
+zsh set_theme.sh
+
+# Makes you use french and english layouts
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'fr')]" > /dev/null 2>&1
 
 # change display and terminal font
 zsh set_font.sh
